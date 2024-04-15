a2585cdd7bf859078471a59d97c2e0811c73b0df
clairenakila18 <155886187+clairenakila18@users.noreply.github.com>
2024-04-15 16:59:55 +0800

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,14 @@
 
 <!-- resources/views/posts/index.blade.php -->
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Posts</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
 <h1>All Posts</h1>
 @if (session('success'))
     <div class="alert alert-success">
@@ -36,3 +44,6 @@
         </tbody>
     </table>
 {{ $posts->links() }}
+
+</body>
+</html>
